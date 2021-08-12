4167d30983a74d6522b0cdb4a6df550f76d94751
Hiro-Aki Hotta <hiroaki.hotta@h6ak.net>
2021-08-13 03:12:55 +0900

    Homeのレイアウトを変更

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import Profile from './Profile';
 import Motto from './Motto';
 import Activity from './Activity';
@@ -12,24 +13,32 @@ const Home: React.FunctionComponent = () => {
       <Typography variant='h1' align='center' gutterBottom>
         Hotta, Hiro-Aki
       </Typography>
-      <Box boxShadow={1} m={2} p={2}>
-        <Typography variant='h5' component='h3' gutterBottom>
-          私は何者？
-        </Typography>
-        <Profile />
-      </Box>
-      <Box boxShadow={1} m={2} p={2}>
-        <Typography variant='h5' component='h3' gutterBottom>
-          座右の銘
-        </Typography>
-        <Motto />
-      </Box>
-      <Box boxShadow={1} m={2} p={2}>
-        <Typography variant='h5' component='h3' gutterBottom>
-          活動（SNS等）
-        </Typography>
-        <Activity />
-      </Box>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+          <Box boxShadow={1} m={2} p={2}>
+            <Typography variant='h5' component='h3' gutterBottom>
+              私は何者？
+            </Typography>
+            <Profile />
+          </Box>
+        </Grid>
+        <Grid xs={6}>
+          <Box boxShadow={1} m={2} p={2}>
+            <Typography variant='h5' component='h3' gutterBottom>
+              座右の銘
+            </Typography>
+            <Motto />
+          </Box>
+        </Grid>
+        <Grid xs={6}>
+          <Box boxShadow={1} m={2} p={2}>
+            <Typography variant='h5' component='h3' gutterBottom>
+              活動（SNS等）
+            </Typography>
+            <Activity />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
